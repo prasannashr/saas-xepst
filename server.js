b6ae7c1d901b175dcf4438be68fd5c5e735b1f8b
unknown <Administrator@tpedge-THINK.(none)>
2015-12-18 11:37:26 +0545

    cahnge in ip in server.js and mongodb Ip

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ var config = require('./config/config')[env];
 require('./subdomain/server/config/mongoose')(config);
 //require('./subdomain/server/config/passport')();
 
-app.use(vhost('54.172.110.156', main))
-app.use(vhost('*. 54.172.110.156', subdomain));
+app.use(vhost('54.229.127.136', main))
+app.use(vhost('*.54.229.127.136', subdomain));
 
 var automatelanguagelabel = require('./subdomain/server/utilities/automatelanguagelabel.js');
 // call function checkAndCreateLabelInDatabase to check multilanguage labels in html files
@@ -42,4 +42,4 @@ automatelanguagelabel.checkAndCreateLabelInDatabase();
 
 
 app.listen(config.port);
-console.log('SAAS XEPST LISTENING ON PORT :' + config.port + ' .....');
+console.log('SAAS XEPST LISTENING ON PORT :' + config.port + ' ..... ENV: '+env);
